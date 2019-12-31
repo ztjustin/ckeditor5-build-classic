feafbf02abeb76919327393456d1f9b269f4e621
Justin Zuniga Torres <justinzu93@gmail.com>
2019-12-30 20:13:07 -0600

    dfgd

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -28,6 +28,7 @@ import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
 import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
+import Base64UploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/base64uploadadapter';
 
 export default class ClassicEditor extends ClassicEditorBase {}
 
@@ -54,11 +55,13 @@ ClassicEditor.builtinPlugins = [
 	Paragraph,
 	PasteFromOffice,
 	Table,
-	TableToolbar
+	TableToolbar,
+	Base64UploadAdapter
 ];
 
 // Editor configuration.
 ClassicEditor.defaultConfig = {
+	plugins: [ Base64UploadAdapter ],
 	toolbar: {
 		items: [
 			'heading',
